70af3548a6dccdba026d55949687e45e2c52ff27
paulrogers <paulr909@proton.me>
2023-07-15 18:52:13 +0100

    reset sizes

--- bulk_process.py
+++ bulk_process.py
@@ -7,8 +7,7 @@ def main():
     images = [file for file in os.listdir() if file.endswith(("jpeg", "jpg", "png"))]
     for image in images:
         img = Image.open(image)
-        # Try 600, 600 for smaller set of images
-        img.thumbnail((800, 800))
+        img.thumbnail((600, 600))
         img.save("resized-" + image, optimize=True, quality=80)
 
 
